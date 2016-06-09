b86a85f523d8b8eb8e0eb2cefa595a6195dd20b0
Tobias Tekampe <ttekampe@klaus-wacker.e5.physik.tu-dortmund.de>
2016-06-09 11:05:10 +0200

    updates in gauss script

--- python/simulation/gauss-ganga.py
+++ python/simulation/gauss-ganga.py
@@ -31,7 +31,7 @@ import os
 attScanPositions = ["a", "c"]
 
 # angle between beam and fibre mat in x
-angle = 30 #int(sys.argv[1])
+angles = [0, 10, 20, 30] #int(sys.argv[1])
 
 local_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
@@ -40,33 +40,34 @@ local_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe
 print local_dir
 
 for pos in attScanPositions:
-    j = Job(application=Gauss(
-            #version="v46r7p2",
-            version="v48r2",
-            #version="v48r3",
-            optsfile=local_dir + "/gauss-job-V5_renato.py",
-            extraopts="execute(\"%s\", %s)"%(pos, angle),
-            user_release_area="/home/ttekampe/cmtuser"
-            #user_release_area=local_dir + "/../cmt",
-            ))
-    j.name = "SF-Pos_" + pos + "_at_" + str(angle)
-    j.outputfiles = [LocalFile("*.sim")]
-    
-    #j.inputfiles=[LocalFile("/home/ttekampe/SciFi/FTv5/DDDB_FTv5_20150424_s20140204_lhcbv38r6.db")]
-    j.inputsandbox =      ['/home/ttekampe/SciFi/FTv5/DDDB_FTv5_20150424_s20140204_lhcbv38r6.db']
-    
-    j.backend=Dirac()
-    #j.backend=Local()
-    #j.backend.settings['BannedSites'] = ['LCG.RRCKI.ru']
-    
-    
-        #events = 10000
-        #eventsperjob = 500
-    
-    events = 10000
-    eventsperjob = 1000
-    j.splitter = GaussSplitter(numberOfJobs=int(round(events*1.00000001/eventsperjob)),
-                               eventsPerJob=eventsperjob)
-    j.postprocessors.append(LHCbFileMerger(files = ['testbeam_simulation_position_' + pos + '_at_' + str(angle) + 'deg.sim'],overwrite=True))
-    j.prepare()
-    j.submit()
+	for angle in angles:
+		j = Job(application=Gauss(
+		        #version="v46r7p2",
+		        version="v48r2",
+		        #version="v48r3",
+		        optsfile=local_dir + "/gauss-job-V5_renato.py",
+		        extraopts="execute(\"%s\", %s)"%(pos, angle),
+		        user_release_area="/home/ttekampe/cmtuser"
+		        #user_release_area=local_dir + "/../cmt",
+		        ))
+		j.name = "SF-Pos_" + pos + "_at_" + str(angle)
+		j.outputfiles = [LocalFile("*.sim")]
+
+		#j.inputfiles=[LocalFile("/home/ttekampe/SciFi/FTv5/DDDB_FTv5_20150424_s20140204_lhcbv38r6.db")]
+		j.inputfiles =      ['/home/ttekampe/SciFi/FTv5/DDDB_FTv5_20150424_s20140204_lhcbv38r6.db']
+
+		j.backend=Dirac()
+		#j.backend=Local()
+		#j.backend.settings['BannedSites'] = ['LCG.RRCKI.ru']
+
+
+		    #events = 10000
+		    #eventsperjob = 500
+
+		events = 10000
+		eventsperjob = 1000
+		j.splitter = GaussSplitter(numberOfJobs=int(round(events*1.00000001/eventsperjob)),
+		                           eventsPerJob=eventsperjob)
+		j.postprocessors.append(LHCbFileMerger(files = ['testbeam_simulation_position_' + pos + '_at_' + str(angle) + 'deg.sim'],overwrite=True))
+		j.prepare()
+		j.submit()
