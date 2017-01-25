d242b397d07119f05e9beff5cd3844fff740ff04
Tobias Tekampe <tobias.tekampe@tu-dortmund.de>
2017-01-25 17:13:13 +0100

    implemented (move) constructors and assignment operators for Cluster

--- include/Cluster.h
+++ include/Cluster.h
@@ -3,34 +3,39 @@
 
 #include <vector>
 
-struct Channel{
+struct Channel {
   unsigned int Uplink;
   unsigned int ChannelNumber;
   double AdcValue;
 };
 
-class Cluster{
-  private:
+class Cluster {
+ private:
   std::vector<Channel> RelatedChannels;
 
-  public:
-  Cluster(){}
-  ~Cluster(){}
-  void Clear() {RelatedChannels.clear();}
-  void AddChannel(const unsigned int uplNumber, const unsigned int chanNumber, const double adcValue);
+ public:
+  Cluster();
+  ~Cluster();
+  Cluster(const Cluster& other);             // copy construction
+  Cluster(Cluster&& other);                  // move construction
+  Cluster& operator=(const Cluster& other);  // copy assignment
+  Cluster& operator=(Cluster&& other);       // move assignment
+
+  void Clear();
+  void AddChannel(const unsigned int uplNumber, const unsigned int chanNumber,
+                  const double adcValue);
   void RemoveChannel(const unsigned int chanNumber);
   void Resize(const unsigned int newsize);
   double GetChargeWeightedMean() const;
   double GetHitWeightedMean() const;
   double GetSumOfAdcValues() const;
   double GetMaximumAdcValue() const;
-  unsigned int GetClusterSize() const {return RelatedChannels.size();}
+  unsigned int GetClusterSize() const;
   unsigned int GetMinChannel() const;
   unsigned int GetMaxChannel() const;
   unsigned int GetSeedChannelNumber() const;
 
-  const std::vector<Channel> &GetRelatedChannels() const {return RelatedChannels;}
-
+  const std::vector<Channel>& GetRelatedChannels() const;
 };
 
-#endif // CLUSTER_H
+#endif  // CLUSTER_H
--- src/Cluster.cpp
+++ src/Cluster.cpp
@@ -1,11 +1,32 @@
 #include "Cluster.h"
 
 #include <iostream>
+#include <utility>
 #include <vector>
 
+Cluster::Cluster() {}
+Cluster::~Cluster() {}
 
+Cluster::Cluster(const Cluster& other)
+    : RelatedChannels(other.RelatedChannels) {}
 
-void Cluster::AddChannel(const unsigned int uplNumber, const unsigned int chanNumber, const double adcValue){
+Cluster::Cluster(Cluster&& other)
+    : RelatedChannels(std::move(other.RelatedChannels)) {}
+
+Cluster& Cluster::operator=(const Cluster& other) {
+  RelatedChannels = other.RelatedChannels;
+  return *this;
+}
+
+Cluster& Cluster::operator=(Cluster&& other) {
+  RelatedChannels = std::move(other.RelatedChannels);
+  return *this;
+}
+
+void Cluster::Clear() { RelatedChannels.clear(); }
+
+void Cluster::AddChannel(const unsigned int uplNumber,
+                         const unsigned int chanNumber, const double adcValue) {
   Channel c;
   c.Uplink = uplNumber;
   c.ChannelNumber = chanNumber;
@@ -13,79 +34,85 @@ void Cluster::AddChannel(const unsigned int uplNumber, const unsigned int chanNu
   RelatedChannels.push_back(c);
 }
 
-void Cluster::RemoveChannel(const unsigned int chanNumber){
-  for(std::vector<Channel>::iterator chan = RelatedChannels.begin(); chan != RelatedChannels.end(); ++chan){
-    if(chan->ChannelNumber == chanNumber){
+void Cluster::RemoveChannel(const unsigned int chanNumber) {
+  for (std::vector<Channel>::iterator chan = RelatedChannels.begin();
+       chan != RelatedChannels.end(); ++chan) {
+    if (chan->ChannelNumber == chanNumber) {
       RelatedChannels.erase(chan);
       return;
     }
   }
-  std::cerr << "Cluster::RemoveChannel: Channel number " << chanNumber << " not in cluster -> cannot remove." << std::endl;
+  std::cerr << "Cluster::RemoveChannel: Channel number " << chanNumber
+            << " not in cluster -> cannot remove." << std::endl;
 }
 
-void Cluster::Resize(const unsigned int newsize){
-  while(RelatedChannels.size() > newsize){
-    std::cout << "current cluster size is " << RelatedChannels.size() << " max size is " << newsize << " -> reducing" << std::endl;
-    if(RelatedChannels.front().AdcValue > RelatedChannels.back().AdcValue) RelatedChannels.pop_back();
-    else RelatedChannels.erase(RelatedChannels.begin());
+void Cluster::Resize(const unsigned int newsize) {
+  while (RelatedChannels.size() > newsize) {
+    std::cout << "current cluster size is " << RelatedChannels.size()
+              << " max size is " << newsize << " -> reducing" << std::endl;
+    if (RelatedChannels.front().AdcValue > RelatedChannels.back().AdcValue)
+      RelatedChannels.pop_back();
+    else
+      RelatedChannels.erase(RelatedChannels.begin());
   }
 }
 
-double Cluster::GetChargeWeightedMean() const{
+double Cluster::GetChargeWeightedMean() const {
   double totalCharge{0.};
   double mean{0.};
-  for(const auto& chan : RelatedChannels){
+  for (const auto& chan : RelatedChannels) {
     mean += static_cast<double>(chan.ChannelNumber) * chan.AdcValue;
     totalCharge += chan.AdcValue;
   }
-  return mean/totalCharge;
+  return mean / totalCharge;
 }
 
-double Cluster::GetHitWeightedMean() const{
+double Cluster::GetHitWeightedMean() const {
   double mean{0.};
-  for(const auto& chan : RelatedChannels){
+  for (const auto& chan : RelatedChannels) {
     mean += chan.ChannelNumber;
   }
-  return mean/static_cast<double>(RelatedChannels.size());
+  return mean / static_cast<double>(RelatedChannels.size());
 }
 
-double Cluster::GetSumOfAdcValues() const{
+double Cluster::GetSumOfAdcValues() const {
   double sumOfAdcValues{0.};
-    for(const auto& chan : RelatedChannels){
+  for (const auto& chan : RelatedChannels) {
     sumOfAdcValues += chan.AdcValue;
   }
   return sumOfAdcValues;
 }
 
-double Cluster::GetMaximumAdcValue() const{
+double Cluster::GetMaximumAdcValue() const {
   double maxAdcValue{0.};
-  for(const auto& chan : RelatedChannels){
-    if(chan.AdcValue > maxAdcValue) maxAdcValue = chan.AdcValue;
+  for (const auto& chan : RelatedChannels) {
+    if (chan.AdcValue > maxAdcValue) maxAdcValue = chan.AdcValue;
   }
   return maxAdcValue;
 }
 
+unsigned int Cluster::GetClusterSize() const { return RelatedChannels.size(); }
 
-unsigned int Cluster::GetMinChannel() const{
+unsigned int Cluster::GetMinChannel() const {
   unsigned int minChannel{9999999};
-  for(const auto& chan : RelatedChannels){
-    if(chan.ChannelNumber < minChannel) minChannel = chan.ChannelNumber;
+  for (const auto& chan : RelatedChannels) {
+    if (chan.ChannelNumber < minChannel) minChannel = chan.ChannelNumber;
   }
   return minChannel;
 }
-unsigned int Cluster::GetMaxChannel() const{
+unsigned int Cluster::GetMaxChannel() const {
   unsigned int maxChannel{0};
-  for(const auto& chan : RelatedChannels){
-    if(chan.ChannelNumber > maxChannel) maxChannel = chan.ChannelNumber;
+  for (const auto& chan : RelatedChannels) {
+    if (chan.ChannelNumber > maxChannel) maxChannel = chan.ChannelNumber;
   }
   return maxChannel;
 }
 
-unsigned int Cluster::GetSeedChannelNumber() const{
+unsigned int Cluster::GetSeedChannelNumber() const {
   double maxAdcValue{0.};
   unsigned int ChannelNumber = 9999;
-  for(const auto& chan : RelatedChannels){
-    if(chan.AdcValue > maxAdcValue){
+  for (const auto& chan : RelatedChannels) {
+    if (chan.AdcValue > maxAdcValue) {
       maxAdcValue = chan.AdcValue;
       ChannelNumber = chan.ChannelNumber;
     }
@@ -93,4 +120,6 @@ unsigned int Cluster::GetSeedChannelNumber() const{
   return ChannelNumber;
 }
 
-
+const std::vector<Channel>& Cluster::GetRelatedChannels() const {
+  return RelatedChannels;
+}
